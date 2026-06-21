0efc6fb0658a3406976004fe0b10add73aac76d7
Dhruv Akbari <ad.dhruvakbari@gmail.com>
2026-06-21 22:49:03 +0530

    Added some api points for the uset

--- src/app.js
+++ src/app.js
@@ -4,20 +4,63 @@ const Users = require("./models/user");
 const app = express();
 const port = 3000;
 
-app.post("/signUp", async (req, res) => {
-  // Createing a new instance of the user model
-  const user = new Users({
-    firstName: "Dhurv",
-    lastName: "Akbari",
-    emailId: "ad.dhruvakbari@gmail.com",
-    password: "1234",
-  });
+app.use(express.json());
 
+app.post("/signUp", async (req, res) => {
+  const user = new Users(req.body);
   try {
     await user.save();
-    res.send("User Added Successfully");
+    res.send("the user created successfully");
   } catch (err) {
-    res.status(400).send("Error on saving the user data");
+    res.status(400).send(`{err}`);
+  }
+});
+
+//! find all the user from the database
+app.get("/findAllUsers", async (req, res) => {
+  const firstName = req.body.firstName;
+  try {
+    const user = await Users.find({ firstName: firstName });
+    user.length !== 0 ? res.send(user) : res.send("User not found");
+  } catch (error) {
+    res.status(400).send("Some went wrong");
+  }
+});
+
+//! get all the users from the database
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await Users.find({});
+    users.length !== 0 ? res.send(users) : res.send("no Users");
+  } catch (error) {
+    res.status(400).send("Some went wrong");
+  }
+});
+
+// !Delete the user
+app.delete("/deleteUser", async (req, res) => {
+  const firstName = req.body.firstName;
+  try {
+    const user = await Users.findOneAndDelete({ firstName: firstName });
+    user.length !== 0 ? res.send(user) : res.send("User Deleted Success");
+  } catch (error) {
+    res.status(400).send("Some went wrong");
+  }
+});
+
+// !update the user
+app.patch("/updateUser", async (req, res) => {
+  const firstName = req.body.firstName;
+  const updatedData = req.body;
+  try {
+    const user = await Users.findOneAndUpdate(
+      { firstName: firstName },
+      updatedData,
+      { returnDocument: "after" },
+    );
+    user.length !== 0 ? res.send(user) : res.send("User Deleted Success");
+  } catch (error) {
+    res.status(400).send("Some went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   lastName: {
     type: String,
   },
-  emailID: {
+  emailId: {
     type: String,
   },
   password: {
