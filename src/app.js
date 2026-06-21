bd3b5d28f22cde80853523cbd72d55dbee8d689d
Dhruv Akbari <ad.dhruvakbari@gmail.com>
2026-06-21 14:54:34 +0530

    added the middleware for the admin route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { adminAuth } = require("./middleware/auth");
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server. Dhruv this side");
+app.use("/admin", adminAuth);
+
+app.get("/admin/data", (req, res) => {
+  res.send("Admin data page");
+});
+
+app.get("/admin/deletdata", (req, res) => {
+  res.send("Admin Delete Data");
 });
 
 app.listen(port, () => {
-  console.log("Server Started on 3000");
+  console.log(`Server Started on ${port}`);
 });
